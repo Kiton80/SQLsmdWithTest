70539185a2c6ad9673aa7556cef2132092438d4d
Kirill <knikitchenko@gmail.com>
2018-05-20 16:52:01 +0300

    добавил сервлеты и настроил томкат

---
+++ src/main/java/Sqlcmd/service/ServiceImpl.java
@@ -0,0 +1,44 @@
+package Sqlcmd.service;
+
+
+
+import main.java.Sqlcmd.model.DatabaseManager;
+import main.java.Sqlcmd.model.JDBCDatabaseManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Kirill on 10.02.2018.
+ */
+public class ServiceImpl implements Service {
+    private DatabaseManager manager;
+
+    public ServiceImpl() {
+        manager = new JDBCDatabaseManager();
+    }
+
+    @Override
+    public List<String> commandsList() {
+        return Arrays.asList("help", "menu", "connect","list");
+    }
+
+    @Override
+    public DatabaseManager connect(String databaseName, String userName, String password) throws Exception {
+       DatabaseManager manager = new JDBCDatabaseManager();
+        manager.connect(databaseName, userName, password);
+        return manager;
+    }
+    @Override
+    public List<String> list(DatabaseManager manager){
+        List<String> result =new ArrayList<>();
+        try {
+            result=manager.getTableNames();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
---
+++ src/main/java/Sqlcmd/web/TestServlet.java
@@ -0,0 +1,77 @@
+package Sqlcmd.web;
+
+
+import Sqlcmd.service.Service;
+import Sqlcmd.service.ServiceImpl;
+import main.java.Sqlcmd.model.DatabaseManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Kirill on 05.05.2018.
+ */
+public class TestServlet extends HttpServlet {
+    private Service service;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        service = new ServiceImpl();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        if (req.getSession().getAttribute("db_manager")==null){
+            req.getRequestDispatcher("connect.jsp").forward(req, resp);
+        }
+
+
+        String action = getAction(req);
+        if (action.startsWith("/menu") || action.equals("/")) {
+            req.setAttribute("items", service.commandsList());
+            req.getRequestDispatcher("menu.jsp").forward(req, resp);
+        } else if (action.startsWith("/help")) {
+            req.getRequestDispatcher("help.jsp").forward(req, resp);
+        } else if (action.startsWith("/list")){
+            req.setAttribute("names", service.list((DatabaseManager) req.getSession().getAttribute("db_manager")));
+            req.getRequestDispatcher("list.jsp").forward(req, resp);
+        } else {
+            req.getRequestDispatcher("error.jsp").forward(req, resp);
+        }
+    }
+    private String getAction(HttpServletRequest req) {
+        String requestURI = req.getRequestURI();
+        return requestURI.substring(req.getContextPath().length(), requestURI.length());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = getAction(req);
+
+        if (req.getSession().getAttribute("db_manager")!=null){
+            req.getRequestDispatcher("is_connected.jsp").forward(req, resp);
+        }
+
+        if (action.startsWith("/connect")) {
+            String databaseName = req.getParameter("dbname");
+            String userName = req.getParameter("username");
+            String password = req.getParameter("password");
+
+            try {
+                service.connect(databaseName, userName, password);
+                DatabaseManager manager = service.connect(databaseName, userName, password);
+                req.getSession().setAttribute("db_manager", manager);
+                resp.sendRedirect(resp.encodeRedirectURL("menu"));
+            } catch (Exception e) {
+                req.setAttribute("message", e.getMessage());
+                req.getRequestDispatcher("error.jsp").forward(req, resp);
+            }
+        }
+    }
+
+}
